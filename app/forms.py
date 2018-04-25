629d27d96f2e29c8e25c3359644e1fb26999ddff
Ahtasham <ch.ahtasham123@gmail.com>
2018-04-26 00:50:43 +0500

    modification Done

--- app/forms.py
+++ app/forms.py
@@ -5,4 +5,4 @@ from .models import Info
 class InfoForm(forms.ModelForm):
     class Meta:
         model = Info
-        fields = ('title',)
+        fields = ('title', 'author', 'price', 'pages', 'book_type', 'publication_date')
--- app/models.py
+++ app/models.py
@@ -4,7 +4,20 @@ from django.db import models
 # Create your models here.
 
 class Info(models.Model):
-    title = models.CharField(max_length=100)
+    HARDCOVER = 1
+    PAPERBACK = 2
+    EBOOK = 3
+    BOOK_TYPE = (
+        (HARDCOVER, 'HardCover'),
+        (PAPERBACK, 'Paperack'),
+        (EBOOK, 'E-book')
+    )
+    title = models.CharField(max_length=50)
+    publication_date = models.DateField(null=True)
+    author = models.CharField(max_length=30, blank=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2,null=True)
+    pages = models.IntegerField(blank=True, null=True)
+    book_type = models.PositiveSmallIntegerField(choices=BOOK_TYPE,default=1)
 
     def __str__(self):
         return self.title
